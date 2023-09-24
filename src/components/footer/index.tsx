359f64fba39f42ff4f33c13fbaa74a4df283dcee
renou <2193239205@qq.com>
2023-09-24 20:43:48 +0800

    视频详情

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,16 @@
 import styles from './index.module.scss'
 function Footer() {
-  return <div className={styles['footer']}>底部</div>
+  return (
+    <div className={styles['footer']}>
+      <p>作者：@Renouc</p>
+      <p>
+        github：
+        <a href="https://github.com/Renouc/blue-ray-cinema" target="blank">
+          https://github.com/Renouc/blue-ray-cinema
+        </a>
+      </p>
+    </div>
+  )
 }
 
 export default Footer
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-
+import zhCN from 'antd/locale/zh_CN'
 import 'normalize.css'
 import '@/assets/css/index.scss'
 import App from './App'
+import { ConfigProvider } from 'antd'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <ConfigProvider locale={zhCN}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </ConfigProvider>
 )
